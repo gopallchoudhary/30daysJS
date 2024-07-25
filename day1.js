d4830fd726f92029a635b20878f73772aeaf9c9f
Gopal Choudhary <gopalchoudhary521@gmail.com>
2024-07-26 01:54:42 +0530

    day1 completed

--- day1.js
+++ day1.js
@@ -1,3 +1,41 @@
 //task-1
-var a = 5;
-console.log(a);
+var num = 5;
+console.log(num);
+
+//task-2
+let str = "javaScript";
+console.log(str);
+
+//task-3
+const bln = false;
+console.log(bln);
+
+//task-4
+const numb = 5;
+const name = "Gopal";
+const signedIn = true;
+const obj = {
+  name: "Hitesh Choudhary",
+  age: 23,
+};
+const arr = [1, 2, 3, 4, 5];
+console.log(
+  "TYPES:- ",
+  typeof numb,
+  typeof name,
+  typeof signedIn,
+  typeof obj,
+  typeof arr
+);
+
+//task-5
+let val = 8;
+console.log("before ", val);
+val = 98;
+console.log("after ", val);
+
+//TASK-6
+const cnt = 10
+console.log(cnt);
+cnt = 74
+console.log(cnt);
